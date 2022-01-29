69595bfd2cc6d1b576a19df2975ea9c0b57b67b5
aiko <88936437+a1k00@users.noreply.github.com>
2022-01-29 18:31:18 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+$('.slide').slick({
+    infinite: true,
+    speed: 2000,
+    fade: true,
+    cssEase: 'ease-in-out',
+    arrows:false,
+    autoplay:true,
+    slidesToShow: 1,
+      slidesToScroll: 1,
+      pauseOnHover:false
+  });
+  
